6e7ecccb25bcb9e6ba6ae3ca2d6d2ce47f7ffe74
Renzo <renzmanri97@gmail.com>
2023-08-24 21:58:48 -0500

    Ultimo Commit

---
+++ bd/common.go
@@ -0,0 +1,77 @@
+package bd
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/renzoemh/gambit/models"
+	"github.com/renzoemh/gambit/secretm"
+)
+
+var SecretModel models.SecretRDSJson
+var err error
+var Db *sql.DB
+
+func ReadSecret() error {
+	SecretModel, err = secretm.GetSecret(os.Getenv("SecretName"))
+	return err
+}
+
+func DbConnect() error {
+	Db, err = sql.Open("mysql", ConnStr(SecretModel))
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	err = Db.Ping()
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Conexion exitosa de la BD")
+	return nil
+}
+
+func ConnStr(claves models.SecretRDSJson) string {
+	var dbUser, authToken, dbEndpoint, dbName string
+	dbUser = claves.Username
+	authToken = claves.Password
+	dbEndpoint = claves.Host
+	dbName = "gambit"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?allowCleartextPasswords=true", dbUser, authToken, dbEndpoint, dbName)
+	fmt.Println(dsn)
+	return dsn
+}
+
+func UserIsAdmin(userUUID string) (bool, string) {
+	fmt.Println("Comienza UserIsAdmin")
+
+	err := DbConnect()
+	if err != nil {
+		return false, err.Error()
+	}
+	defer Db.Close()
+
+	sentencia := "SELECT 1 FROM users WHERE User_UUID='" + userUUID + "' AND User_Status = 0"
+	fmt.Println(sentencia)
+
+	rows, err := Db.Query(sentencia)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	var valor string
+	rows.Next()
+	rows.Scan(&valor)
+
+	fmt.Println("UserIsAdmin > Ejecucion exitosa - valor devuelto " + valor)
+	if valor == "1" {
+		return true, ""
+	}
+
+	return false, "User is not Admin"
+}
---
+++ main.go
@@ -0,0 +1,61 @@
+package gambit
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/renzoemh/gambit/awsgo"
+	"github.com/renzoemh/gambit/bd"
+	"github.com/renzoemh/gambit/handlers"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) (*events.APIGatewayProxyResponse, error) {
+	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		panic("Error en los parametros, debe enciar 'SecretName', 'UrlPrefix'")
+	}
+
+	var res *events.APIGatewayProxyResponse
+	path := strings.Replace(request.RawPath, os.Getenv("UrlPrefix"), "", -1)
+	method := request.RequestContext.HTTP.Method
+	body := request.Body
+	header := request.Headers
+
+	bd.ReadSecret()
+
+	status, message := handlers.Manejadores(path, method, body, header, request)
+
+	headersResp := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	res = &events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       string(message),
+		Headers:    headersResp,
+	}
+
+	return res, nil
+}
+
+func ValidoParametros() bool {
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	return traeParametro
+}
